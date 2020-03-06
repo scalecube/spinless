589f8d35052d6d001c92b11e290f1d98276ed642
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2020-03-06 16:03:55 +0200

    Feature: spinnaker application template

--- libs/spinnaker_api.py
+++ libs/spinnaker_api.py
@@ -1,29 +1,37 @@
 import subprocess
-from flask import Flask, request
-
-
-
-app = Flask(__name__)
+from jinja2 import Environment, FileSystemLoader
 
 
 class SpinnakerPipeline:
     def __init__(self, data):
         self.data = data
 
+    def application_create(self, application_name):
+        with open("/opt/spinnaker/app.yaml", "w") as chart_file:
+            j2_env = Environment(loader=FileSystemLoader("/opt/spinnaker/templates/"))
+            gen_template = j2_env.get_template(
+                'app_template.j2').render(application_name=application_name)
+            chart_file.write(gen_template)
+        proc = subprocess.Popen(["spin application save --file /opt/spinnaker/app.yaml"],
+                                stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        return
+
     def pipeline_create(self):
-        app.logger.info("Request to pipeline_create is {}".format(self.data))
+        # app.logger.info("Request to pipeline_create is {}".format(self.data))
         application = "{}-{}".format(self.data["owner"], self.data["repo"])
         pipeline_name = "deploy"
-        cmd = "spin application save"
+        self.application_create(application)
+
         return {}
 
     def pipeline_deploy(self):
-        app.logger.info("Request to pipeline_deploy {}".format(self.data))
+        # app.logger.info("Request to pipeline_deploy {}".format(self.data))
         repo = self.data['repo']
-        return { "eventId" :"" }
+        return {"eventId":""}
 
     def pipeline_cancel(self):
-        app.logger.info("Request to pipeline_cancel {}".format(self.data))
+        # app.logger.info("Request to pipeline_cancel {}".format(self.data))
         application = "{}-{}".format(self.data["owner"], self.data["repo"])
         pipeline_id = self.data['id']
         return {}
