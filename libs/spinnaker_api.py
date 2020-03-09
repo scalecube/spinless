4aea759d049f415c2505cdfe9ff515122ba34de5
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2020-03-09 14:49:01 +0200

    Feature: deploy pipeline template

--- libs/spinnaker_api.py
+++ libs/spinnaker_api.py
@@ -1,4 +1,5 @@
 import subprocess
+import uuid
 from jinja2 import Environment, FileSystemLoader
 
 
@@ -8,12 +9,12 @@ class SpinnakerPipeline:
         self.logger.info("SpinnakerPipeline init")
         self.data = data
 
-    def application_create(self, application_name):
-        with open("/opt/spinnaker/app.yaml", "w") as chart_file:
+    def application_create(self, application):
+        with open("/opt/spinnaker/app.yaml", "w") as app_file:
             j2_env = Environment(loader=FileSystemLoader("/opt/spinnaker/templates/"))
             gen_template = j2_env.get_template(
-                'app_template.j2').render(application_name=application_name)
-            chart_file.write(gen_template)
+                'app.j2').render(application_name=application)
+            app_file.write(gen_template)
         proc = subprocess.Popen(["spin application save --file /opt/spinnaker/app.yaml"],
                                 stdout=subprocess.PIPE, shell=True)
         (out, err) = proc.communicate()
@@ -25,11 +26,32 @@ class SpinnakerPipeline:
 
     def pipeline_create(self):
         # app.logger.info("Request to pipeline_create is {}".format(self.data))
-        application = "{}-{}".format(self.data["owner"], self.data["repo"])
-        pipeline_name = "deploy"
+        owner = self.data["owner"]
+        repo = self.data["repo"]
+        application = "{}-{}".format(owner, repo)
         self.application_create(application)
-
-        return {}
+        with open("/opt/spinnaker/deploy_pipeline.yaml", "w") as pipeline_file:
+            j2_env = Environment(loader=FileSystemLoader("/opt/spinnaker/templates/"))
+            gen_template = j2_env.get_template(
+                'deploy_pipeline.j2').render(
+                application=application,
+                helm_package=application,
+                repo_slug=repo,
+                default_artifact_uuid=uuid.uuid1(),
+                artifact_uuid=uuid.uuid1(),
+                match_artifact_uuid=uuid.uuid1(),
+                stages_default_artifact_uuid=uuid.uuid1(),
+                stages_artifact_uuid=uuid.uuid1(),
+                stages_match_artifact_uuid=uuid.uuid1())
+            pipeline_file.write(gen_template)
+        proc = subprocess.Popen(["spin pipeline save --file /opt/spinnaker/deploy_pipeline.yaml"],
+                                stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        self.logger.info("Creation of deploy pipeline output: {}".format(
+            out.decode("utf-8") if out else None))
+        self.logger.info("Creation of deploy pipeline error: {}".format(
+            err.decode("utf-8") if err else None))
+        return
 
     def pipeline_deploy(self):
         # app.logger.info("Request to pipeline_deploy {}".format(self.data))
