8d6c2d5d4bbdc095d6eb85ca3d886bcf3ee9cf44
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2020-03-26 13:06:35 +0200

    Feature: spinless self env

--- libs/helm_api.py
+++ libs/helm_api.py
@@ -1,14 +1,23 @@
+import time
+import os
 from subprocess import Popen, PIPE
 
 from libs.vault_api import Vault
 
 
 class Helm:
-    def __init__(self):
+    def __init__(self, logger):
+        self.logger = logger
+        self.timestamp = round(time.time())
         pass
 
     def get_env_from_vault(self):
-        return {}
+        vault = Vault(logger=self.logger,
+                      vault_server=os.getenv("VAULT_ADDR"),
+                      service_role=os.getenv("VAULT_ROLE"),
+                      vault_secrets_path=os.getenv("VAULT_SECRETS_PATH")
+                      )
+        return vault.get_self_app_env()
 
     def sum_all_env(self):
         env_from_vault = self.get_env_from_vault()
