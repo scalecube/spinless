bf9879e60fc85693efc1e5d8558e257ce8b6d3b7
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2020-03-27 14:51:19 +0200

    Test

--- libs/vault_api.py
+++ libs/vault_api.py
@@ -43,8 +43,9 @@ class Vault:
                 self.root_path, self.app_path, env_or_app)
         self.logger.info("Get_env in vault path is: {}".format(path))
         try:
-            env = client.read(path)['data']
-            return env
+            env = client.read(path)
+            self.logger("ENV from vault is {}: ".format(env))
+            return env['data']
         except Exception as e:
             self.logger.info("Vault get_env exception is: {}".format(e))
             return {}
