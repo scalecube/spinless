c47f81f0038c9ecb845d730900081dd90843e393
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2020-04-08 12:22:21 +0300

    Feature: configmap to add

--- infrastructure/modules/aws/eks.tf
+++ infrastructure/modules/aws/eks.tf
@@ -24,3 +24,27 @@ output "kubeconfig-certificate-authority-data" {
   value = aws_eks_cluster.eks.certificate_authority.0.data
 }
 
+locals {
+  config_map_aws_auth = <<CONFIGMAPAWSAUTH
+
+
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${aws_iam_role.eks-node.arn}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+CONFIGMAPAWSAUTH
+
+}
+
+output "config_map_aws_auth" {
+  value = local.config_map_aws_auth
+}
+
