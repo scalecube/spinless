fc334257960eb5c88e09e71f7d1593295a59cf6c
Sergey Ripa <snripa@gmail.com>
2020-04-13 11:31:41 +0200

    fix path

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 /usr/local/bin/helm init --client-only --tiller-namespace=tiller
-cd "$TF_STATE" && /usr/local/bin/terraform init "$TF_WORKING_DIR"
+cd "$APP_WORKING_DIR/$TF_STATE" && /usr/local/bin/terraform init "$APP_WORKING_DIR/$TF_WORKING_DIR"
 cd "$APP_WORKING_DIR" && python ./start_app.py
