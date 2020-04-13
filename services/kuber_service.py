41d7430ea968485a689718219d9dda9d18501931
Sergey Ripa <snripa@gmail.com>
2020-04-13 15:08:31 +0200

    get secrets from vault in order to create cloud from profile

--- services/kuber_service.py
+++ services/kuber_service.py
@@ -3,17 +3,27 @@ from libs.infrastructure import TF
 from libs.kube_api import KctxApi
 from libs.vault_api import Vault
 
+DEFAULT_CLOUD = {"type": "eks", "name": "default"}
 
-def kube_cluster_create(job_ref, applogger):
+
+def kube_cluster_create(job_ref, app_logger):
     try:
         data = job_ref.data
         job_ref.emit("RUNNING: Start cluster creation job: {}".format(data.get("namespace")), None)
 
         # use vault later
-        vault = Vault(logger=applogger)
-        kctx_api = KctxApi(vault, applogger)
-        terraform = TF(applogger, data["aws_region"], data["aws_access_key"], data["aws_secret_key"],
-                       data["cluster_name"], data["az1"], data["az2"], data["kube_nodes_amount"],
+        vault = Vault(logger=app_logger)
+
+        cloud_provider_api = CloudApi(vault, app_logger)
+        kctx_api = KctxApi(vault, app_logger)
+        cloud_profile_req = data.get("cloud_profile", DEFAULT_CLOUD)
+        cloud_profile = cloud_provider_api.get_cloud_provider(cloud_profile_req)
+        cluster_name = data["cluster_name"]
+        job_ref.emit("RUNNING: using cloud profile:{} to create cluster: {}".format(cloud_profile_req, cluster_name),
+                     None)
+        terraform = TF(app_logger, cloud_profile["aws_region"], cloud_profile["aws_access_key"],
+                       cloud_profile["aws_secret_key"],
+                       cluster_name, cloud_profile["az1"], cloud_profile["az2"], cloud_profile["kube_nodes_amount"],
                        data["kube_nodes_instance_type"], kctx_api)
 
         for (msg, res) in terraform.install_kube():
