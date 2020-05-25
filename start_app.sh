fe2803fa45ed688667605eba9799bf11fdf87caa
Serhii Kolomiiets <serhii.kolomiiets@jivygroup.com>
2020-05-25 15:47:39 +0300

    Fix: paths

--- start_app.sh
+++ start_app.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+sed -i "s/tf_token/$TF_VAR_token/g" "$TF_CLI_CONFIG_FILE"
 cd "$TF_STATE" && /usr/local/bin/terraform init "$APP_WORKING_DIR/$TF_WORKING_DIR"
 cd "$APP_WORKING_DIR" && python ./api.py
