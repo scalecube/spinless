cd47d40ef57ad83d14c2b813dd12cc8ed498e142
Sergey Ripa <snripa@gmail.com>
2020-06-05 13:56:15 +0200

    get service versions api. fixes #51

--- app/common/shell.py
+++ app/common/shell.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 
-def shell_await(cmd, env=None, with_output=False, cwd=None, timeout=None):
+def shell_await(cmd, env=None, with_output=False, cwd=None, timeout=None, get_stream=False):
     """
     Execute a command in new Subprocess (Popen(...))
     :param cmd: command to execute (using Popen)
@@ -18,6 +18,12 @@ def shell_await(cmd, env=None, with_output=False, cwd=None, timeout=None):
     else:
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=cwd)
 
+    def stream():
+        if not with_output:
+            return None
+        else:
+            return p.stdout
+
     def output():
         if not with_output:
             yield "Command started: {}".format(cmd)
@@ -28,4 +34,7 @@ def shell_await(cmd, env=None, with_output=False, cwd=None, timeout=None):
                     break
                 yield line.rstrip().decode("utf-8")
 
-    return p.wait(timeout=timeout), output()
+    if get_stream:
+        return p.wait(timeout=timeout), stream()
+    else:
+        return p.wait(timeout=timeout), output()
