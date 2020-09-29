cb7bc2c38d01e3c58e9158bc6507f1a419906db7
Sergii Lypnytskyi <sergii.lypnytskyi@jivygroup.com>
2020-09-29 13:55:39 +0300

    Added vars

--- app/infra/templates/template_tfvars.tf
+++ app/infra/templates/template_tfvars.tf
@@ -1,4 +1,5 @@
 cluster-name = "{{ variables.resource_name }}"
+cluster_type = "{{ variables.cluster_type }}"
 network_id = "{{ variables.network_id }}"
 nebula_cidr_block = "{{ variables.nebula_cidr_block }}"
 nebula_route_table_id = "{{ variables.nebula_route_table_id }}"
@@ -7,9 +8,12 @@ peer_vpc_id = "{{ variables.peer_vpc_id }}"
 certificate_arn_ext = "{{ variables.certificate_arn_ext }}"
 certificate_arn_discovery = "{{ variables.certificate_arn_discovery }}"
 nodePools = {{ variables.nodePools }}
-{% if variables.cluster_type %}
-cluster_type = "{{ variables.cluster_type }}"
-{% endif %}
 {% if variables.eks_version %}
 eks-version = "{{ variables.eks_version }}"
+{% endif %}
+{% if variables.transit_gw_id %}
+transit_gw_id = "{{ variables.transit_gw_id }}"
+{% endif %}
+{% if variables.ops_vpc_id %}
+ops_vpc_id = "{{ variables.ops_vpc_id }}"
 {% endif %}
