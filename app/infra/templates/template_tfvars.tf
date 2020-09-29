be2060e66296128287ddf9a4b9fd23fc19c5e068
Sergii Lypnytskyi <sergii.lypnytskyi@jivygroup.com>
2020-09-29 15:21:02 +0300

    Edit tfvars

--- app/infra/templates/template_tfvars.tf
+++ app/infra/templates/template_tfvars.tf
@@ -1,5 +1,4 @@
 cluster-name = "{{ variables.resource_name }}"
-cluster_type = "{{ variables.cluster_type }}"
 network_id = "{{ variables.network_id }}"
 nebula_cidr_block = "{{ variables.nebula_cidr_block }}"
 nebula_route_table_id = "{{ variables.nebula_route_table_id }}"
@@ -8,12 +7,9 @@ peer_vpc_id = "{{ variables.peer_vpc_id }}"
 certificate_arn_ext = "{{ variables.certificate_arn_ext }}"
 certificate_arn_discovery = "{{ variables.certificate_arn_discovery }}"
 nodePools = {{ variables.nodePools }}
+cluster_type = "{{ variables.cluster_type }}"
+transit_gw_id = "{{ variables.transit_gw_id }}"
+ops_vpc_id = "{{ variables.ops_vpc_id }}"
 {% if variables.eks_version %}
 eks-version = "{{ variables.eks_version }}"
-{% endif %}
-{% if variables.transit_gw_id %}
-transit_gw_id = "{{ variables.transit_gw_id }}"
-{% endif %}
-{% if variables.ops_vpc_id %}
-ops_vpc_id = "{{ variables.ops_vpc_id }}"
 {% endif %}
