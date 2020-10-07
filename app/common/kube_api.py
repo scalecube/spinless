f17478e872ffef1e09e3bf6998982274076f339a
monergeim <monergeim@gmail.com>
2020-10-07 21:53:16 +0300

    kuber ext snat install

--- app/common/kube_api.py
+++ app/common/kube_api.py
@@ -212,6 +212,10 @@ class KctxApi:
                   f'--namespace cluster-autoscaler'
         return self.execute_command(command, kube_env)
 
+    def setup_ext_snat(self, kube_env):
+        command = "kubectl set env daemonset -n kube-system aws-node AWS_VPC_K8S_CNI_EXTERNALSNAT=true"
+        return self.execute_command(command, kube_env)
+
     def setup_traefik(self, kube_env):
         """
         Setup traefik plugin in created cluster
