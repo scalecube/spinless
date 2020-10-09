06ea7e2fc8df5f556258734ff20a7a5172f163b8
Serg Li <monergeim@gmail.com>
2020-10-09 15:41:54 +0300

    changed template

--- app/infra/templates/template_tfvars.tf
+++ app/infra/templates/template_tfvars.tf
@@ -1,11 +1,11 @@
 cluster-name = "{{ variables.resource_name }}"
 network_id = "{{ variables.network_id }}"
 certificate_arn_ext = "{{ variables.certificate_arn_ext }}"
-nodePools = {{ variables.nodePools }}
 cluster_type = "{{ variables.cluster_type }}"
 transit_gw_id = "{{ variables.transit_gw_id }}"
 head_vpc_id = "{{ variables.head_vpc_id }}"
 dns_suffix = "{{ variables.dns_suffix }}"
+nodePools = {{ variables.nodePools }}
 {% if variables.eks_version %}
 eks-version = "{{ variables.eks_version }}"
 {% endif %}
