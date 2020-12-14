53cc2d6baef4a5aea5aca5d66780653ddd6aad1c
Anton Lysenok <anton.lysenok@jivygroup.com>
2020-12-14 20:00:23 +0200

    Add support for dns_lb terraform variable

--- app/infra/templates/template_tfvars.tf
+++ app/infra/templates/template_tfvars.tf
@@ -5,6 +5,7 @@ cluster_type = "{{ variables.cluster_type }}"
 transit_gw_id = "{{ variables.transit_gw_id }}"
 head_vpc_id = "{{ variables.head_vpc_id }}"
 dns_suffix = "{{ variables.dns_suffix }}"
+dns_lb = "{{ variables.dns_lb }}"
 nodePools = {{ variables.nodePools }}
 {% if variables.eks_version %}
 eks-version = "{{ variables.eks_version }}"
